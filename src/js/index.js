c1e1282865491bf9455090bfaf10461cc80f1d8e
fincci <lp-oliveirac@hotmail.com>
2022-03-15 03:47:03 -0300

    add sound and minor adjust

--- src/js/index.js
+++ src/js/index.js
@@ -2,12 +2,14 @@
 const listaSelecaoPokemon = document.querySelectorAll('.pokemon')
 const pokemonsCards = document.querySelectorAll('.cartao-pokemon')
 
+var audioMouseOver = document.getElementsByClassName('soundMouseOver')[0];
+var audioClick = document.getElementsByClassName('soundClick')[0];
+
 listaSelecaoPokemon.forEach(pokemon => {
 
     pokemon.addEventListener('click', () => {
         const cartaoPokemonAberto = document.querySelector('.aberto')
         cartaoPokemonAberto.classList.remove('aberto')
-
         const idPokemonSelecionado = pokemon.attributes.id.value
         const idCardSelecionado = 'card-' + idPokemonSelecionado
         const cartaoPokemonAbrir = document.getElementById(idCardSelecionado)
@@ -19,7 +21,16 @@ listaSelecaoPokemon.forEach(pokemon => {
         const listaPokemonPosterior = document.getElementById(idPokemonSelecionado)
         listaPokemonPosterior.classList.add('ativo')
         listaPokemonPosterior.classList.remove('desativado')
-
     })
+
+    pokemon.addEventListener('mouseenter', () => {
+        audioMouseOver.volume = 0.1;
+        audioMouseOver.play();
+    })
+    pokemon.addEventListener('click', () => {
+        audioClick.volume = 0.1;
+        audioClick.play();
+    })
+    
 })
 
